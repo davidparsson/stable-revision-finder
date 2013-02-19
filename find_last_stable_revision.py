8e09e728ba2603ae6d81194107a735115c2e1cb4
David Pärsson <david@parsson.se>
2013-02-19 16:17:00 +0100

    Fixed indentation, blank lines and imports according to Python style guidelines

--- find_last_stable_revision.py
+++ find_last_stable_revision.py
@@ -6,122 +6,128 @@ import optparse
 
 verbose = False
 
+
 class RevisionStatuses():
 
-  STABLE = 1
-  BUILDING = 2
-  NOT_BUILT = 3
-  UNSTABLE = 4
+    STABLE = 1
+    BUILDING = 2
+    NOT_BUILT = 3
+    UNSTABLE = 4
 
-  def __init__(self):
-    self._revision_results = {}
+    def __init__(self):
+        self._revision_results = {}
 
-  def add_stable_revision(self, revision):
-    self._revision_results[revision] = self.STABLE
+    def add_stable_revision(self, revision):
+        self._revision_results[revision] = self.STABLE
 
-  def add_unstable_revision(self, revision):
-    if self._revision_results.get(revision) != self.STABLE:
-      self._revision_results[revision] = self.UNSTABLE
+    def add_unstable_revision(self, revision):
+        if self._revision_results.get(revision) != self.STABLE:
+            self._revision_results[revision] = self.UNSTABLE
 
-  def add_building_revision(self, revision):
-    self._revision_results[revision] = self.BUILDING
+    def add_building_revision(self, revision):
+        self._revision_results[revision] = self.BUILDING
 
-  def is_revision_stable(self, revision):
-    while revision > 0:
-      if revision in self._revision_results.keys():
-        return self._revision_results[revision] == self.STABLE
-      revision -= 1
-    return None
+    def is_revision_stable(self, revision):
+        while revision > 0:
+            if revision in self._revision_results.keys():
+                return self._revision_results[revision] == self.STABLE
+            revision -= 1
+        return None
 
-  def get_status_for(self, revision):
-    return self._revision_results.get(revision, self.NOT_BUILT)
+    def get_status_for(self, revision):
+        return self._revision_results.get(revision, self.NOT_BUILT)
 
-  def get_status_as_text_for(self, revision):
-    dictionary = {self.STABLE: "STABLE",
-                  self.UNSTABLE: "UNSTABLE",
-                  self.BUILDING: "BUILDING",
-                  self.NOT_BUILT: "NOT BUILT"}
-    return dictionary[self.get_status_for(revision)]
+    def get_status_as_text_for(self, revision):
+        dictionary = {self.STABLE: "STABLE",
+                                    self.UNSTABLE: "UNSTABLE",
+                                    self.BUILDING: "BUILDING",
+                                    self.NOT_BUILT: "NOT BUILT"}
+        return dictionary[self.get_status_for(revision)]
 
 
 def parse(url, tree=None):
-  if url[-1] != "/":
-    url += "/"
-  url = "%sapi/python" % url
-  if tree:
-    url += "?tree=%s" % tree
-  return ast.literal_eval(urllib.urlopen(url).read())
+    if url[-1] != "/":
+        url += "/"
+    url = "%sapi/python" % url
+    if tree:
+        url += "?tree=%s" % tree
+    return ast.literal_eval(urllib.urlopen(url).read())
+
 
 def is_stable_revision(eligible_revision, revisions_by_job):
-  any_stable = False
-  for job_name in revisions_by_job.keys():
-    revision_statuses = revisions_by_job[job_name]
-    stable = revision_statuses.is_revision_stable(eligible_revision)
-    if stable is not None:
-      if not stable:
-        status = revision_statuses.get_status_as_text_for(eligible_revision)
-        print_if_verbose("Revision %d is %s for %s!" % (eligible_revision, status, job_name))
-        return False
-      any_stable = True
-  return any_stable
+    any_stable = False
+    for job_name in revisions_by_job.keys():
+        revision_statuses = revisions_by_job[job_name]
+        stable = revision_statuses.is_revision_stable(eligible_revision)
+        if stable is not None:
+            if not stable:
+                status = revision_statuses.get_status_as_text_for(eligible_revision)
+                print_if_verbose("Revision %d is %s for %s!" % (eligible_revision, status, job_name))
+                return False
+            any_stable = True
+    return any_stable
+
 
 def get_highest_stable_revision(eligible_revisions, revisions_by_job):
-  eligible_revisions.sort(reverse=True)
-  for eligible_revision in eligible_revisions:
-    if is_stable_revision(eligible_revision, revisions_by_job):
-      return eligible_revision
-  return -1
+    eligible_revisions.sort(reverse=True)
+    for eligible_revision in eligible_revisions:
+        if is_stable_revision(eligible_revision, revisions_by_job):
+            return eligible_revision
+    return -1
+
 
 def find_revision(url, verbose=False):
-  view_details = parse(url, "jobs[name,url]")
+    view_details = parse(url, "jobs[name,url]")
 
-  revisions_by_job = {}
-  eligible_revisions = set()
-  for job in view_details['jobs']:
-    print_if_verbose("Querying %s..." % job['name'])
-    result = parse(job['url'], "builds[building,result,changeSet[items[revision]]]")
+    revisions_by_job = {}
+    eligible_revisions = set()
+    for job in view_details['jobs']:
+        print_if_verbose("Querying %s..." % job['name'])
+        result = parse(job['url'], "builds[building,result,changeSet[items[revision]]]")
 
-    revision_statuses = RevisionStatuses()
-    revisions_by_job[job['name']] = revision_statuses
+        revision_statuses = RevisionStatuses()
+        revisions_by_job[job['name']] = revision_statuses
 
-    for build in result['builds']:
-      if build['building']:
-        for item in build['changeSet']['items']:
-          revision_statuses.add_building_revision(item['revision'])
-      elif build['result'] == 'SUCCESS':
-        for item in build['changeSet']['items']:
-          revision = item['revision']
-          revision_statuses.add_stable_revision(revision)
-          eligible_revisions.add(revision)
-      else:
-        for item in build['changeSet']['items']:
-          revision_statuses.add_unstable_revision(item['revision'])
+        for build in result['builds']:
+            if build['building']:
+                for item in build['changeSet']['items']:
+                    revision_statuses.add_building_revision(item['revision'])
+            elif build['result'] == 'SUCCESS':
+                for item in build['changeSet']['items']:
+                    revision = item['revision']
+                    revision_statuses.add_stable_revision(revision)
+                    eligible_revisions.add(revision)
+            else:
+                for item in build['changeSet']['items']:
+                    revision_statuses.add_unstable_revision(item['revision'])
 
-  return get_highest_stable_revision(list(eligible_revisions), revisions_by_job)
+    return get_highest_stable_revision(list(eligible_revisions), revisions_by_job)
+
 
 def print_if_verbose(message):
-  if verbose:
-    print message
+    if verbose:
+        print message
+
 
 def main():
-  global verbose
-  parser = optparse.OptionParser(usage="""Usage: %prog VIEW_URL [options]
+    global verbose
+    parser = optparse.OptionParser(usage="""Usage: %prog VIEW_URL [options]
 
 Gets the highest common stable revision for all jobs in the supplied Jenkins view.""")
-  parser.add_option("-v", "--verbose", help="Prints progress, instead of only the revision", action="store_true", default=False)
-  try:
-    (options, (url,)) = parser.parse_args()
-    verbose = options.verbose
-    revision = find_revision(url)
-    if verbose:
-      print
-      print "Last stable revision: %d" % revision
-    else:
-      print revision
-    return 0
-  except ValueError:
-    parser.print_help()
-    return 1
+    parser.add_option("-v", "--verbose", help="Prints progress, instead of only the revision", action="store_true", default=False)
+    try:
+        (options, (url,)) = parser.parse_args()
+        verbose = options.verbose
+        revision = find_revision(url)
+        if verbose:
+            print
+            print "Last stable revision: %d" % revision
+        else:
+            print revision
+        return 0
+    except ValueError:
+        parser.print_help()
+        return 1
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
--- tests.py
+++ tests.py
@@ -5,134 +5,134 @@
 #
 import unittest
 import xmlrunner
-from mockito import mock, when, contains, any
+from mockito import mock, when, contains
 from find_last_stable_revision import *
 import find_last_stable_revision
 
 
 class AcceptanceTest(unittest.TestCase):
 
-  urllib = mock()
+    urllib = mock()
 
-  def setUp(self):
-    find_last_stable_revision.urllib = self.urllib
-    self. number_of_jobs = 0
+    def setUp(self):
+        find_last_stable_revision.urllib = self.urllib
+        self. number_of_jobs = 0
 
-  def test_does_not_select_buildling_revision(self):
-    self.given_job_with_builds(build(2), build(1))
-    self.given_job_with_builds(build(2, building=True), build(1))
-    self.assertEqual(1, find_last_stable_revision.find_revision("url"))
+    def test_does_not_select_buildling_revision(self):
+        self.given_job_with_builds(build(2), build(1))
+        self.given_job_with_builds(build(2, building=True), build(1))
+        self.assertEqual(1, find_last_stable_revision.find_revision("url"))
 
-  def test_does_not_select_revision_after_buildling_revision(self):
-    self.given_job_with_builds(build(3), build(1))
-    self.given_job_with_builds(build(2, building=True), build(1))
-    self.assertEqual(1, find_last_stable_revision.find_revision("url"))
+    def test_does_not_select_revision_after_buildling_revision(self):
+        self.given_job_with_builds(build(3), build(1))
+        self.given_job_with_builds(build(2, building=True), build(1))
+        self.assertEqual(1, find_last_stable_revision.find_revision("url"))
 
-  def test_does_not_select_unstable_revision(self):
-    self.given_job_with_builds(build(2), build(1))
-    self.given_job_with_builds(build(2, stable=False), build(1))
-    self.assertEqual(1, find_last_stable_revision.find_revision("url"))
+    def test_does_not_select_unstable_revision(self):
+        self.given_job_with_builds(build(2), build(1))
+        self.given_job_with_builds(build(2, stable=False), build(1))
+        self.assertEqual(1, find_last_stable_revision.find_revision("url"))
 
-  def test_does_not_select_revision_after_unstable_build(self):
-    self.given_job_with_builds(build(3), build(1))
-    self.given_job_with_builds(build(2, stable=False), build(1))
-    self.assertEqual(1, find_last_stable_revision.find_revision("url"))
+    def test_does_not_select_revision_after_unstable_build(self):
+        self.given_job_with_builds(build(3), build(1))
+        self.given_job_with_builds(build(2, stable=False), build(1))
+        self.assertEqual(1, find_last_stable_revision.find_revision("url"))
 
-  def test_selects_stable_revision_after_unstable_build(self):
-    self.given_job_with_builds(build(2), build(1))
-    self.given_job_with_builds(build(3), build(2, stable=False), build(1))
-    self.assertEqual(3, find_last_stable_revision.find_revision("url"))
+    def test_selects_stable_revision_after_unstable_build(self):
+        self.given_job_with_builds(build(2), build(1))
+        self.given_job_with_builds(build(3), build(2, stable=False), build(1))
+        self.assertEqual(3, find_last_stable_revision.find_revision("url"))
 
-  def test_selects_highest_stable_revision(self):
-    self.given_job_with_builds(build(3), build(1))
-    self.given_job_with_builds(build(2), build(1))
-    self.assertEqual(3, find_last_stable_revision.find_revision("url"))
+    def test_selects_highest_stable_revision(self):
+        self.given_job_with_builds(build(3), build(1))
+        self.given_job_with_builds(build(2), build(1))
+        self.assertEqual(3, find_last_stable_revision.find_revision("url"))
 
-  def test_selects_revision_even_if_not_built(self):
-    self.given_job_with_builds(build(1))
-    self.given_job_with_builds()
-    self.assertEqual(1, find_last_stable_revision.find_revision("url"))
+    def test_selects_revision_even_if_not_built(self):
+        self.given_job_with_builds(build(1))
+        self.given_job_with_builds()
+        self.assertEqual(1, find_last_stable_revision.find_revision("url"))
 
-  def test_does_not_selects_revision_when_none_built(self):
-    self.given_job_with_builds()
-    self.given_job_with_builds()
-    self.assertEqual(-1, find_last_stable_revision.find_revision("url"))
+    def test_does_not_selects_revision_when_none_built(self):
+        self.given_job_with_builds()
+        self.given_job_with_builds()
+        self.assertEqual(-1, find_last_stable_revision.find_revision("url"))
 
-  def test_selects_highest_revision_when_multiple_changes(self):
-    self.given_job_with_builds(build([3, 2, 1]))
-    self.assertEqual(3, find_last_stable_revision.find_revision("url"))
+    def test_selects_highest_revision_when_multiple_changes(self):
+        self.given_job_with_builds(build([3, 2, 1]))
+        self.assertEqual(3, find_last_stable_revision.find_revision("url"))
 
-  def test_selects_highest_revision_when_not_sorted(self):
-    self.given_job_with_builds(build(2), build([1, 7, 3]), build(4))
-    self.given_job_with_builds(build(6), build(2), build(5))
-    self.assertEqual(7, find_last_stable_revision.find_revision("url"))
+    def test_selects_highest_revision_when_not_sorted(self):
+        self.given_job_with_builds(build(2), build([1, 7, 3]), build(4))
+        self.given_job_with_builds(build(6), build(2), build(5))
+        self.assertEqual(7, find_last_stable_revision.find_revision("url"))
 
-  def given_job_with_builds(self, *builds):
-    url = "/job%d/api/python?tree=builds" % self.number_of_jobs
-    response = '{"builds":[%s]}' % ','.join(builds)
+    def given_job_with_builds(self, *builds):
+        url = "/job%d/api/python?tree=builds" % self.number_of_jobs
+        response = '{"builds":[%s]}' % ','.join(builds)
 
-    self.number_of_jobs += 1
+        self.number_of_jobs += 1
 
-    self.given_response_for_url(url, response)
-    self.given_number_of_jobs(self.number_of_jobs)
+        self.given_response_for_url(url, response)
+        self.given_number_of_jobs(self.number_of_jobs)
 
-  def given_number_of_jobs(self, number_of_jobs):
-    response = '{"jobs":[%s]}' % ','.join([job(i) for i in range(number_of_jobs)])
-    self.given_response_for_url("?tree=jobs", response)
+    def given_number_of_jobs(self, number_of_jobs):
+        response = '{"jobs":[%s]}' % ','.join([job(i) for i in range(number_of_jobs)])
+        self.given_response_for_url("?tree=jobs", response)
 
-  def given_response_for_url(self, url, response):
-    open_url = mock()
-    when(self.urllib).urlopen(contains(url)).thenReturn(open_url)
-    when(open_url).read().thenReturn(response)
+    def given_response_for_url(self, url, response):
+        open_url = mock()
+        when(self.urllib).urlopen(contains(url)).thenReturn(open_url)
+        when(open_url).read().thenReturn(response)
 
 
 class TestRevisionStatuses(unittest.TestCase):
 
-  def setUp(self):
-    self.revision_statuses = RevisionStatuses()
+    def setUp(self):
+        self.revision_statuses = RevisionStatuses()
 
-  def test_not_stable_when_no_added_revisions(self):
-    self.assertIsNone(self.revision_statuses.is_revision_stable(1))
+    def test_not_stable_when_no_added_revisions(self):
+        self.assertIsNone(self.revision_statuses.is_revision_stable(1))
 
-  def test_added_stable_revision_is_stable(self):
-    self.revision_statuses.add_stable_revision(1)
-    self.assertTrue(self.revision_statuses.is_revision_stable(1))
+    def test_added_stable_revision_is_stable(self):
+        self.revision_statuses.add_stable_revision(1)
+        self.assertTrue(self.revision_statuses.is_revision_stable(1))
 
-  def test_revision_after_added_stable_revision_is_stable(self):
-    self.revision_statuses.add_stable_revision(1)
-    self.assertTrue(self.revision_statuses.is_revision_stable(2))
+    def test_revision_after_added_stable_revision_is_stable(self):
+        self.revision_statuses.add_stable_revision(1)
+        self.assertTrue(self.revision_statuses.is_revision_stable(2))
 
-  def test_revision_before_added_stable_revision_not_stable(self):
-    self.revision_statuses.add_stable_revision(2)
-    self.assertIsNone(self.revision_statuses.is_revision_stable(1))
+    def test_revision_before_added_stable_revision_not_stable(self):
+        self.revision_statuses.add_stable_revision(2)
+        self.assertIsNone(self.revision_statuses.is_revision_stable(1))
 
-  def test_added_unstable_revision_is_not_stable(self):
-    self.revision_statuses.add_stable_revision(1)
-    self.revision_statuses.add_unstable_revision(2)
-    self.assertFalse(self.revision_statuses.is_revision_stable(2))
+    def test_added_unstable_revision_is_not_stable(self):
+        self.revision_statuses.add_stable_revision(1)
+        self.revision_statuses.add_unstable_revision(2)
+        self.assertFalse(self.revision_statuses.is_revision_stable(2))
 
-  def test_revision_is_stable_if_stable_once(self):
-    self.revision_statuses.add_unstable_revision(1)
-    self.revision_statuses.add_stable_revision(1)
-    self.revision_statuses.add_unstable_revision(1)
-    self.assertTrue(self.revision_statuses.is_revision_stable(1))
+    def test_revision_is_stable_if_stable_once(self):
+        self.revision_statuses.add_unstable_revision(1)
+        self.revision_statuses.add_stable_revision(1)
+        self.revision_statuses.add_unstable_revision(1)
+        self.assertTrue(self.revision_statuses.is_revision_stable(1))
 
 
 def build(revisions=[], building=False, stable=True):
-  if stable:
-    result = 'SUCCESS'
-  else:
-    result = 'FAILURE'
+    if stable:
+        result = 'SUCCESS'
+    else:
+        result = 'FAILURE'
 
-  if type(revisions) != list:
-    revisions = [revisions]
+    if type(revisions) != list:
+        revisions = [revisions]
 
-  return str({'building': building, 'result': result, 'changeSet': {'items': [{'revision': revision} for revision in revisions]}})
+    return str({'building': building, 'result': result, 'changeSet': {'items': [{'revision': revision} for revision in revisions]}})
 
 
 def job(number):
-  return str({'name': 'job%d' % number, 'url': 'http://jenkins/job/job%d/' % number})
+    return str({'name': 'job%d' % number, 'url': 'http://jenkins/job/job%d/' % number})
 
 
 if __name__ == '__main__':
-  unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
